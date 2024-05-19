5bea9f9ae45b3f94c60eaee9e77495419cac00bc
Odya01 <republiquez25@gmail.com>
2024-05-19 15:30:58 +0300

    fix burger btn

--- css & js/script.js
+++ css & js/script.js
@@ -1,9 +1,7 @@
-const burgerBtn = document.querySelector('.link__burger');
-const burgerIcon = document.querySelector('.link__icon-burger');
+const burgerBtn = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.nav__burger');
 
 burgerBtn.addEventListener('click', ()=>{
     burgerMenu.classList.toggle('active');
-    burgerIcon.classList.toggle('link__icon-burger_active');
     document.body.classList.toggle('overflow');
 });
